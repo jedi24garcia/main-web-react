8c820d4ff4f11827ee2e2c0bfb6b8996fa8b7307
jedi24garcia <jedi24garcia@gmail.com>
2024-02-05 09:45:04 +1300

    created main page

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+
+function Home() {
+    return (
+        <div className="MainTitle">
+            <h1>Welcome to my personal website</h1>
+        </div>
+    );
+}
+
+export default Home;
