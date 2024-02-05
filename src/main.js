43ad5f560d586d7fa8d11849ecb4d89d55622aaf
jedi24garcia <jedi24garcia@gmail.com>
2024-02-05 21:49:43 +1300

    adde p

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 function Home() {
     return (
-        <div className='flex items-center justify-center h-screen'>
+        <div className='bg-gradient-to-r from-teal-400 via-teal-500 to-black flex items-center justify-center h-screen'>
             <div className='text-center'> 
-                <h1 className='bg-teal-500 text-7xl'>Welcome to my personal website</h1>
+                <h1 className='text-7xl'>Welcome to my personal website</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non elit scelerisque, ultrices libero vitae, pretium nibh. Vivamus neque nisl, cursus sit amet eros id, tristique consectetur libero.</p>
             </div>
         </div>
     );
