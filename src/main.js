bff3b041e44084288358ebc5d476f9f2e26f07d0
jedi24garcia <jedi24garcia@gmail.com>
2024-05-02 15:51:03 +1200

    updated greetings

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className='bg-gradient-to-r from-teal-400 via-teal-500 to-black flex items-center justify-center h-screen'>
             <div className='text-center'> 
-                <h1 className='text-7xl'>Welcome to my personal website!</h1>
+                <h1 className='text-7xl'>Kia ora and welcome to my personal website!</h1>
             </div>
         </div>
     );
