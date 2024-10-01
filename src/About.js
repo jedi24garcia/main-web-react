39560b6a8b69658f0800d67904772a71519e70f8
jedi24garcia <jedi24garcia@gmail.com>
2024-10-01 15:51:06 +1300

    modified two pages

--- src/About.js
+++ src/About.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
+import consoleImage from './/images/consoles.jpg';
+
 function About() {
     return (
         <div>
             <header class="flex justify-between items-center p-20 bg-gradient-to-r from-neutral-400 to-gray-500" />
             <div class='item-body px-60 py-20'>
                 <h1 class='font-serif text-5xl'>about</h1>
-                <p class='item-body py-10'>Hello<br />
+                <p class='item-body py-5'>Hello there,<br />
                     Just a dedicated programmer living in Wellington, New Zealand.<br />
                     Beyond of the world of coding, I love to explore new places and cultures,<br />
                     which combines my love for traveling with a curiosity of learning.<br />
                     I always seek new adventures in both my career and personal life.
                 </p>
-            </div>    
+            </div>
+            <div class='flex justify-center items-center pt-12'>
+                <img src={consoleImage} alt='' />
+            </div>
+            <div>
+                <img class='h-48 px-20 py-10' src={consoleImage} alt='' />
+            </div>
         </div>
     );
 };
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,20 +7,24 @@ function Portfolio() {
     return (
         <div>
             <header class="flex justify-between items-center p-20 bg-gradient-to-r from-neutral-400 to-gray-500" />
-                <div class='item-body px-60 py-20'>
+                <div class='item-body px-60 py-10'>
                     <h1 class='font-serif text-5xl'>portfolio</h1>
                 </div>
+                <p class='item-body px-60'>I've had the privilege of exploring some incredible destinations, each one leveling<br />
+                its unique mark on my journey. From breathtaking landscapes to rich cultural experiences, traveling has been<br />
+                a source of inspiration and growth for me. Please enjoy a few of the unforgettable places I've visited.
+                </p> 
                 <div class='scene'>
                     <div class='imageStyle'>
-                    <img style={{ '--i': '000deg' }} src={consoleImage} alt="Portfolio project 1" />
-                    <img style={{ '--i': '040deg' }} src={consoleImage} alt="Portfolio project 2" />
-                    <img style={{ '--i': '080deg' }} src={consoleImage} alt="Portfolio project 3" />
-                    <img style={{ '--i': '120deg' }} src={consoleImage} alt="Portfolio project 4" />
-                    <img style={{ '--i': '160deg' }} src={consoleImage} alt="Portfolio project 5" />
-                    <img style={{ '--i': '200deg' }} src={consoleImage} alt="Portfolio project 6" />
-                    <img style={{ '--i': '240deg' }} src={consoleImage} alt="Portfolio project 7" />
-                    <img style={{ '--i': '280deg' }} src={consoleImage} alt="Portfolio project 8" />
-                    <img style={{ '--i': '320deg' }} src={consoleImage} alt="Portfolio project 9" />
+                        <img style={{ '--i': '000deg' }} src={consoleImage} alt="Portfolio project 1" />
+                        <img style={{ '--i': '040deg' }} src={consoleImage} alt="Portfolio project 2" />
+                        <img style={{ '--i': '080deg' }} src={consoleImage} alt="Portfolio project 3" />
+                        <img style={{ '--i': '120deg' }} src={consoleImage} alt="Portfolio project 4" />
+                        <img style={{ '--i': '160deg' }} src={consoleImage} alt="Portfolio project 5" />
+                        <img style={{ '--i': '200deg' }} src={consoleImage} alt="Portfolio project 6" />
+                        <img style={{ '--i': '240deg' }} src={consoleImage} alt="Portfolio project 7" />
+                        <img style={{ '--i': '280deg' }} src={consoleImage} alt="Portfolio project 8" />
+                        <img style={{ '--i': '320deg' }} src={consoleImage} alt="Portfolio project 9" />
                     </div>
                 </div>
         </div>
