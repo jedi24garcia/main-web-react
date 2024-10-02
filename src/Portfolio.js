dd4a3ba7bf4cd921ec32d4903ac54e0796fddecd
jedi24garcia <jedi24garcia@gmail.com>
2024-10-03 11:29:50 +1300

    modified poftfolio page

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,6 +5,7 @@ import FooterPage from './Footer';
 
 import './Portfolio.css';
 import consoleImage from './/images/consoles.jpg';
+import aboutImage from './/images/Image.jpg';
 
 function Portfolio() {
     return (
@@ -19,16 +20,33 @@ function Portfolio() {
                 </p> 
                 <div className='scene'>
                     <div className='imageStyle'>
-                        <img style={{ '--i': '000deg' }} src={consoleImage} alt="Portfolio project 1" />
+                        <img style={{ '--i': '000deg' }} src={aboutImage} alt="Portfolio project 1" />
                         <img style={{ '--i': '040deg' }} src={consoleImage} alt="Portfolio project 2" />
-                        <img style={{ '--i': '080deg' }} src={consoleImage} alt="Portfolio project 3" />
+                        <img style={{ '--i': '080deg' }} src={aboutImage} alt="Portfolio project 3" />
                         <img style={{ '--i': '120deg' }} src={consoleImage} alt="Portfolio project 4" />
-                        <img style={{ '--i': '160deg' }} src={consoleImage} alt="Portfolio project 5" />
+                        <img style={{ '--i': '160deg' }} src={aboutImage} alt="Portfolio project 5" />
                         <img style={{ '--i': '200deg' }} src={consoleImage} alt="Portfolio project 6" />
-                        <img style={{ '--i': '240deg' }} src={consoleImage} alt="Portfolio project 7" />
+                        <img style={{ '--i': '240deg' }} src={aboutImage} alt="Portfolio project 7" />
                         <img style={{ '--i': '280deg' }} src={consoleImage} alt="Portfolio project 8" />
-                        <img style={{ '--i': '320deg' }} src={consoleImage} alt="Portfolio project 9" />
+                        <img style={{ '--i': '320deg' }} src={aboutImage} alt="Portfolio project 9" />
                     </div>
+                </div>
+                    <div className='item-body px-60 py-10'>
+                    <h1 className='font-serif text-5xl'>projects</h1>
+                </div>
+                <p className='item-body px-60'>Below are some of my personal projects, each reflecting my passion for creativity<br />
+                and coding. From web applications to mobile development, these projects showcase my journey through the world of<br />
+                technology. Take a moment to explore, dive into details, and enjoy the work that reflects my creativity as a developer.<br />
+                </p>
+                <div className='flex justify-between px-60 py-10'>
+                    <section className='item-body w-1/2 px-5'>
+                        <h2>Project 1</h2>
+                        <img src={consoleImage} alt='' />
+                    </section>
+                    <section className='item-body w-1/2 px-5'>
+                        <h2>Project 2</h2>
+                        <img src={consoleImage} alt='' />
+                    </section>
                 </div>
             <FooterPage />
         </div>
