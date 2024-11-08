dca6fa72ea6e3cce230cb4ec8ae8ecbcbd4ba4e3
jedi24garcia <jedi24garcia@gmail.com>
2024-11-09 11:58:21 +1300

    updated images in portfolio page

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,6 +6,14 @@ import FooterPage from './Footer';
 import './Portfolio.css';
 import consoleImage from './/images/consoles.jpg';
 import aboutImage from './/images/Image.jpg';
+import boholImage from './/images/Bohol.jpg';
+import carImage from './/images/Car.jpg';
+import makaraImage from './/images/Makara.jpg';
+import nycImage from './/images/NYC.jpg';
+import ruapehuImage from './/images/Ruapehu.jpg';
+import seaImage from './/images/Sea.jpg';
+import shibuyaImage from './/images/Shibuya.jpg';
+import universalImage from './/images/Universal.jpg';
 import firstProject from './/images/mainbg.webp';
 
 function Portfolio() {
@@ -22,14 +30,14 @@ function Portfolio() {
                 <div className='scene'>
                     <div className='imageStyle'>
                         <img style={{ '--i': '000deg' }} src={aboutImage} alt="Portfolio project 1" />
-                        <img style={{ '--i': '040deg' }} src={aboutImage} alt="Portfolio project 2" />
-                        <img style={{ '--i': '080deg' }} src={aboutImage} alt="Portfolio project 3" />
-                        <img style={{ '--i': '120deg' }} src={aboutImage} alt="Portfolio project 4" />
-                        <img style={{ '--i': '160deg' }} src={aboutImage} alt="Portfolio project 5" />
-                        <img style={{ '--i': '200deg' }} src={aboutImage} alt="Portfolio project 6" />
-                        <img style={{ '--i': '240deg' }} src={aboutImage} alt="Portfolio project 7" />
-                        <img style={{ '--i': '280deg' }} src={aboutImage} alt="Portfolio project 8" />
-                        <img style={{ '--i': '320deg' }} src={aboutImage} alt="Portfolio project 9" />
+                        <img style={{ '--i': '040deg' }} src={boholImage} alt="Portfolio project 2" />
+                        <img style={{ '--i': '080deg' }} src={carImage} alt="Portfolio project 3" />
+                        <img style={{ '--i': '120deg' }} src={makaraImage} alt="Portfolio project 4" />
+                        <img style={{ '--i': '160deg' }} src={nycImage} alt="Portfolio project 5" />
+                        <img style={{ '--i': '200deg' }} src={ruapehuImage} alt="Portfolio project 6" />
+                        <img style={{ '--i': '240deg' }} src={seaImage} alt="Portfolio project 7" />
+                        <img style={{ '--i': '280deg' }} src={shibuyaImage} alt="Portfolio project 8" />
+                        <img style={{ '--i': '320deg' }} src={universalImage} alt="Portfolio project 9" />
                     </div>
                 </div>
                 <div className='item-body px-60 py-10'>
