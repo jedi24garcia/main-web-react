d21bbe4c5ae3ae12a44cba69d53bdfb92b98621d
jedi24garcia <jedi24garcia@gmail.com>
2024-11-14 11:36:44 +1300

    moved portfolio images to about page

--- src/About.js
+++ src/About.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 import Home from './main';
 import FooterPage from './Footer';
+import './About.css';
 
 import aboutImage from './/images/Image.jpg';
+import cookImage from './/images/Image.jpg';
+import boholImage from './/images/Bohol.jpg';
+import carImage from './/images/Car.jpg';
+import makaraImage from './/images/Makara.jpg';
+import nycImage from './/images/NYC.jpg';
+import ruapehuImage from './/images/Ruapehu.jpg';
+import seaImage from './/images/Sea.jpg';
+import shibuyaImage from './/images/Shibuya.jpg';
+import universalImage from './/images/Universal.jpg';
 
 function About() {
     return (
@@ -19,8 +29,25 @@ function About() {
                 </p>
             </div>
             <div className='flex justify-center items-center mb-12'>
-                <img src={aboutImage} className="object-cover opacity-75 rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" style={{ width: '500px' }} alt='' />
+                <img src={aboutImage} className="object-cover opacity-75 rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" style={{ width: '400px' }} alt='' />
             </div>
+            <p className='item-body px-60 font-tr2n font-extrabold'>I've had the privilege of exploring some incredible destinations, each one leveling<br />
+                its unique mark on my journey. From breathtaking landscapes to rich cultural experiences, traveling has been<br />
+                a source of inspiration and growth for me. Please enjoy a few of the unforgettable places I've visited.
+            </p> 
+            <div className='scene'>
+                    <div className='imageStyle'>
+                        <img style={{ '--i': '000deg' }} src={cookImage} alt="Portfolio project 1" />
+                        <img style={{ '--i': '040deg' }} src={boholImage} alt="Portfolio project 2" />
+                        <img style={{ '--i': '080deg' }} src={carImage} alt="Portfolio project 3" />
+                        <img style={{ '--i': '120deg' }} src={makaraImage} alt="Portfolio project 4" />
+                        <img style={{ '--i': '160deg' }} src={nycImage} alt="Portfolio project 5" />
+                        <img style={{ '--i': '200deg' }} src={ruapehuImage} alt="Portfolio project 6" />
+                        <img style={{ '--i': '240deg' }} src={seaImage} alt="Portfolio project 7" />
+                        <img style={{ '--i': '280deg' }} src={shibuyaImage} alt="Portfolio project 8" />
+                        <img style={{ '--i': '320deg' }} src={universalImage} alt="Portfolio project 9" />
+                    </div>
+                </div>
             <FooterPage />
         </div>
     );
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,17 +3,7 @@ import React from 'react';
 import Home from './main';
 import FooterPage from './Footer';
 
-import './Portfolio.css';
 import consoleImage from './/images/consoles.jpg';
-import cookImage from './/images/Image.jpg';
-import boholImage from './/images/Bohol.jpg';
-import carImage from './/images/Car.jpg';
-import makaraImage from './/images/Makara.jpg';
-import nycImage from './/images/NYC.jpg';
-import ruapehuImage from './/images/Ruapehu.jpg';
-import seaImage from './/images/Sea.jpg';
-import shibuyaImage from './/images/Shibuya.jpg';
-import universalImage from './/images/Universal.jpg';
 import firstProject from './/images/mainbg.webp';
 
 function Portfolio() {
@@ -21,45 +11,28 @@ function Portfolio() {
         <div className='bg-sky-900'>
             <Home />
             <div className='item-body px-60 py-10'>
-                <h1 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>portfolio</h1>
+                <h1 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>projects</h1>
             </div>
-                <p className='item-body px-60 font-tr2n font-extrabold'>I've had the privilege of exploring some incredible destinations, each one leveling<br />
-                its unique mark on my journey. From breathtaking landscapes to rich cultural experiences, traveling has been<br />
-                a source of inspiration and growth for me. Please enjoy a few of the unforgettable places I've visited.
-                </p> 
-                <div className='scene'>
-                    <div className='imageStyle'>
-                        <img style={{ '--i': '000deg' }} src={cookImage} alt="Portfolio project 1" />
-                        <img style={{ '--i': '040deg' }} src={boholImage} alt="Portfolio project 2" />
-                        <img style={{ '--i': '080deg' }} src={carImage} alt="Portfolio project 3" />
-                        <img style={{ '--i': '120deg' }} src={makaraImage} alt="Portfolio project 4" />
-                        <img style={{ '--i': '160deg' }} src={nycImage} alt="Portfolio project 5" />
-                        <img style={{ '--i': '200deg' }} src={ruapehuImage} alt="Portfolio project 6" />
-                        <img style={{ '--i': '240deg' }} src={seaImage} alt="Portfolio project 7" />
-                        <img style={{ '--i': '280deg' }} src={shibuyaImage} alt="Portfolio project 8" />
-                        <img style={{ '--i': '320deg' }} src={universalImage} alt="Portfolio project 9" />
-                    </div>
-                </div>
-                <div className='item-body px-60 py-10'>
-                    <h1 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>projects</h1>
-                </div>
-                <p className='item-body px-60 font-tr2n font-extrabold'>Below are some of my personal projects, each reflecting my passion for creativity<br />
-                and coding. From web applications to mobile development, these projects showcase my journey through the world of<br />
-                technology. Take a moment to explore, dive into details, and enjoy the work that reflects my creativity as a developer.<br />
-                </p>
-                <div className='flex justify-between px-60 py-10'>
-                    <section className='item-body w-1/2 px-5'>
-                        <h2 className='mb-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white'>Project 1</h2>
-                        <a href='https://github.com/jedi24garcia/Fitness-App/tree/master' rel=''>
-                            <img src={firstProject} className="object-cover rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" alt='' />
-                        </a>
-                        <p className='py-10 font-tr2n font-extrabold'>This is a mobile application to guide individuals who are just starting out at the gym!</p>
-                    </section>
-                    <section className='item-body w-1/2 px-5'>
-                        <h2 className='mb-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white'>Project 2</h2>
-                        <img src={consoleImage} className="object-cover rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" alt='' />
-                    </section>
-                </div>
+            <p className='item-body px-60 font-tr2n font-extrabold'>Below are some of my personal projects, each reflecting my passion for creativity<br />
+            and coding. From web applications to mobile development, these projects showcase my journey through the world of<br />
+            technology. Take a moment to explore, dive into details, and enjoy the work that reflects my creativity as a developer.<br />
+            </p>
+            <div className='flex justify-between px-60 py-10'>
+                <section className='item-body w-1/2 px-5'>
+                    <h2 className='mb-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white'>Project 1</h2>
+                    <a href='https://github.com/jedi24garcia/Fitness-App/tree/master' rel=''>
+                        <img src={firstProject} className="w-full aspect-video object-cover rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" alt='' />
+                    </a>
+                    <p className='py-10 font-tr2n font-extrabold'>This is a mobile application using React-Native to guide individuals who are just starting out at the gym!</p>
+                </section>
+                <section className='item-body w-1/2 px-5'>
+                    <h2 className='mb-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white'>Project 2</h2>
+                    <a href='https://github.com/jedi24garcia/robotech' rel=''>
+                        <img src={consoleImage} className="w-full aspect-video object-cover rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" alt='' />
+                    </a>
+                    <p className='py-10 font-tr2n font-extrabold'>A Python program that lets the user use their voice to open a web browser. When a user say something like 'open Google' or 'open Youtube,' it listens, recognizes the command, and opens up the browser to the website they've asked for.</p>
+                </section>
+            </div>
             <FooterPage />
         </div>
     );
