ceac6fdad0c02fb204d382e20b2869347eddb5e0
jedi24garcia <jedi24garcia@gmail.com>
2025-01-23 11:08:54 +1300

    fixed Welcome design

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import './Home.css';
 import HeaderPage from './HeaderPage';
 import FooterPage from './FooterPage';
 
@@ -13,12 +14,6 @@ function Home() {
                     <img src={profileImage} className="object-cover rounded-full hover:scale-110 transform transition duration-500" style={{ width: '400px' }} alt='' /> 
                     <div id="logo" className='item-body px-20'>Welcome!</div>  
                 </div>
-                <div id="screen-container">
-                    <canvas id="canvas"></canvas>
-                    <div class="blue cyclotron"></div>
-                    <div class="skyblue cyclotron"></div>
-                    <div class="greetings">Hello, User!</div>
-                </div>   
             <FooterPage />
         </div>
     );
