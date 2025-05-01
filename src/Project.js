acb8d07e9bc39222e8a79220ffea8872f89eac64
jedi24garcia <jedi24garcia@gmail.com>
2025-05-02 11:34:24 +1200

    replaced robo project with pacifica project

--- src/Project.js
+++ src/Project.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Home from './HeaderPage';
 import FooterPage from './FooterPage';
 
-import roboImage from './/images/robo.jpg';
+import pacificImage from './/images/pacific.JPG';
 import firstProject from './/images/mainbg.webp';
 import schoolImage from './/images/school.jpg';
 
@@ -25,16 +25,14 @@ function Project() {
                     <p className='py-10 font-medium text-sky-400/100'>This is a mobile application using React-Native to guide individuals who are just starting out at the gym!</p>
                 </section>
                 <section className='item-body w-1/2 px-5'>
-                    <h2 className='mb-4 text-2xl tracking-tight font-tr2n text-gray-900 text-sky-400/100'>Robotech</h2>
-                    <a href='https://github.com/jedi24garcia/robotech/blob/master/airobo/self.py' rel=''>
-                        <img src={roboImage} className="w-full aspect-video object-cover rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" alt='' />
+                    <h2 className='mb-4 text-2xl tracking-tight font-tr2n text-gray-900 text-sky-400/100'>Pacifica Project</h2>
+                    <a href='https://github.com/jedi24garcia/Pacifica_Project' rel=''>
+                        <img src={pacificImage} className="w-full aspect-video object-cover rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" alt='' />
                     </a>
-                    <p className='py-10 font-medium text-sky-400/100'>A Python program that lets the user use their voice to open a web browser using Speech-Recognition. When a user say something like 'open Google' or 'open Youtube,' it listens, recognizes the command, and opens up the browser to the website they've asked for. It is still a work
-                        in progress.
-                    </p>
+                    <p className='py-10 font-medium text-sky-400/100'>Serves as a comprehensive educational and research tool that enables users to survey, comprehend, and understands the cultural heritage customs, and etiquette of Pacific Culture.</p>
                 </section>
                 <section className='item-body w-1/2 px-5'>
-                    <h2 className='mb-4 text-2xl tracking-tight font-tr2n text-gray-900 text-sky-400/100'>School_Database</h2>
+                    <h2 className='mb-4 text-2xl tracking-tight font-tr2n text-gray-900 text-sky-400/100'>School Database</h2>
                     <a href='https://github.com/jedi24garcia/School_Database/blob/master/main.py' rel=''>
                         <img src={schoolImage} className="w-full aspect-video object-cover rounded-lg hover:scale-110 transform transition duration-500 border-4 border-cyan-400" alt='' />
                     </a>
