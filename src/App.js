69da935f860b766a5552b3fe3709431208b3550f
jedi24garcia <jedi24garcia@gmail.com>
2025-05-05 22:11:43 +1200

    Fix case issue and rebuild

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<Project />} />
         <Route path="/contact" element={<Contact />} />
