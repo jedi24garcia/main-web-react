0b9acdb951355d7c805753d81928ef305b4c288a
jedi24garcia <jedi24garcia@gmail.com>
2025-05-05 22:03:01 +1200

    modified

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<Project />} />
         <Route path="/contact" element={<Contact />} />
